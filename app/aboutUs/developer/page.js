812e15764f2d499a441b5fa215f58bfc056bbf7d
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-10-20 11:55:24 +0530

    Making a new developer page using a nested route for about us page , URL = /aboutus/developer.

---
+++ app/aboutUs/developer/page.js
@@ -0,0 +1,9 @@
+//ourDomain.com/aboutUs/developer
+const About = () => {
+    return (
+      <div>
+        This NextJS app is built by Somya Bhatngar!
+      </div>
+    )
+  }
+  export default About;
