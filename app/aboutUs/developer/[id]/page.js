faf4463ea8094f75980c5dfac4950c383887c462
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-10-21 11:25:12 +0530

    making a component to display the names of all the developers and show there details on clicking them which is still not working properly.

---
+++ app/aboutUs/developer/[id]/page.js
@@ -0,0 +1,26 @@
+// ourDomain.com/aboutUs/developer/[id]
+
+const Developer = ({ params }) => {
+    const developerDetails = {
+        1: { name: 'Yash', role: 'Senior Developer', bio: 'Experienced in building robust applications.' },
+        2: { name: 'Vaibhav', role: 'Backend Developer', bio: 'Specialized in designing efficient backend systems.' },
+        3: { name: 'Suresh', role: 'Frontend Developer', bio: 'Passionate about creating intuitive user interfaces.' }
+    };
+
+    const developer = developerDetails[params.developer];
+
+    return (
+        <div>
+            <h1>Developer Details</h1>
+            {developer && (
+                <div>
+                    <h2>Name: {developer.name}</h2>
+                    <p>Role: {developer.role}</p>
+                    <p>Bio: {developer.bio}</p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Developer;
--- app/aboutUs/developer/page.js
+++ app/aboutUs/developer/page.js
@@ -1,9 +1,28 @@
 //ourDomain.com/aboutUs/developer
-const About = () => {
+
+import Link from 'next/link';
+
+const DeveloperList = () => {
+    const details = [
+        { id: 1, name: 'Yash', role: 'Senior Developer' },
+        { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+        { id: 3, name: 'Suresh', role: 'Frontend Developer' }
+    ];
+
     return (
-      <div>
-        This NextJS app is built by Somya Bhatngar!
-      </div>
-    )
-  }
-  export default About;
+        <div>
+            <h1>List of Developers :- </h1>
+            <ul>
+                {details.map((developer) => (
+                    <li key={developer.id}>
+                        <Link href={`/aboutUs/developer/${developer.id}`}>
+                            {developer.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default DeveloperList;
